6873d36080169658f1ed5f22e70ecfdb74d38b65
inehadev <156919946+inehadev@users.noreply.github.com>
2024-03-03 23:44:38 +0530

    complted userPost component and passed as props to the userPage

--- Components/UserPost.jsx
+++ Components/UserPost.jsx
@@ -6,10 +6,12 @@ import Action from './Action'
 const UserPost = ({postIMG , postTitle , likes , replies}) => {
     const[liked, setliked] = useState(false);
   return (
+    <>
     <Link to={'/markzuckerberg/post/i'}>
         <Flex gap={3} mb={4} py={3}>
             <Flex flexDirection={"column"} alignItems={"center"}>
                 <Avatar size="md" name="Mark Zuckerberg" src='/zuck-avatar.png' />
+
                     <Box w="1px" h={"full"} bg={"gray"} my={2} > </Box>
                         <Box position={"relative"} w={"full"}>
                         <Avatar
@@ -41,50 +43,56 @@ const UserPost = ({postIMG , postTitle , likes , replies}) => {
 						/>
 
                     </Box>
+                    
 
             </Flex>
+            
             <Flex  flex={1} flexDirection={"column"} gap={2}>
                 <Flex justifyContent={"space-between"} w={"full"}>
                     <Flex w={"full"} alignItems={"center"}>
-                        <Text fontsize = {"sm"} fontweinght={"bold"}>
+                        <Text fontsize = {"sm"} fontWeinght={"bold"}>
                             markzuckerberg
                         </Text>
                         <Image src='/verified.png' w={4} h={4} ml={1}/>
-                        <Text fontStyle={"sm"} color={"gray"}>
-                            1d
-                        </Text>
-                        <BsThreeDots/>
+
+                        </Flex>
+                       
+                       <Flex gap={4} alignItems={"center"}>
+							<Text fontStyle={"sm"} color={"gray.light"}>
+								1d
+							</Text>
+							<BsThreeDots />
+						</Flex>
                         
-                    </Flex>
                 </Flex>
+
                 <Text fontSize={"sm"}>This is my first post</Text>
                 
+                {postIMG && (
                     <Box borderRadius={6} overflow={"hidden"} border={"1px solid"} borderColor={"gray"}>
-                    <Image src='/post1.png' alt='Post Image' w="full" />
-                    </Box>
-               
-
-                {/* <Flex gap={3} my={1}>
-                    <Actions liked={liked} setliked={setliked} />
-                </Flex>
-                <Flex gap={2} alignItems={"center"}>
-                <Text color={"gray.light"} fontSize='sm' >
-                {replies} replies
-            </Text>
-            <Box w={0.5} h={0.5} borderRadius={"full"} bg={"gray"}>
-                <Text color={"gray"} fontsize='sm'>
-                    {likes} likes
-                </Text>
-            </Box>
-                </Flex> */}
+                    <Image src={postIMG} w="full" />
+                    </Box> )}
 
                 <Flex gap={3} my={1}></Flex>
-                {/* <Action/> */}
+                <Action liked={liked} setliked={setliked} />
+                </Flex>
 
-            </Flex>
-        </Flex>
+                <Flex ml={10} gap={3} alignItems={"center"}>
+                    <Text color={"gray"} fontSize={"sm"}>{replies} replies</Text>
+                    <Box w={0.5 } borderRadius={"full"} bg={"gray"}></Box>
+                    <Text color={"gray"} fontSize={"sm"}>{likes} likes</Text>
+                </Flex>
+                </Flex>
+               
+                
+                </Link>
 
-    </Link>
+
+
+
+
+               
+            </>
   )
 }
 
--- Pages/Userpage.jsx
+++ Pages/Userpage.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-import Header from "../Components/Header";
 import UserHeader from "../Components/UserHeader";
 import UserPost from "../Components/UserPost";
 export default function Userpage (){
     return(
         <>
          <UserHeader/>
-         <UserPost/>
-
+         <UserPost likes={1200} replies={481} postIMG='/post1.png' postTitle={"Let's talk about Threads"}/>
+         <UserPost likes={1200} replies={481} postIMG='/post2.png' postTitle={"Let's talk about Threads"}/>
+         <UserPost likes={1200} replies={481} postIMG='/post3.png' postTitle={"Let's talk about Threads"}/>
         </>
     )
 }
