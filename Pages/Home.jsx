2f280545ba774c1fbf17cf6d4935cdf30ea73157
inehadev <156919946+inehadev@users.noreply.github.com>
2024-04-19 22:56:22 +0530

    update

--- Pages/Home.jsx
+++ Pages/Home.jsx
@@ -1,13 +1,37 @@
 import react, { useEffect } from 'react';
 import CreatePost from '../Components/CreatePost';
+import { Link } from 'react-router-dom';
+import { Button, Flex } from '@chakra-ui/react';
 
 const Home = ()=>{
+    useEffect (()=>{
+        const getfeedpost =async()=>{
+            try {
+                const res = await fetch ('/:postid');
+                console.log(res);
+                
+            } catch (error) {
+                console.log(error)
+                
+            }
+
+        }
+        getfeedpost();
+
+    } , [])
+   
     
     return(
-        <div><CreatePost/>
+        <>
+        <Link to={'/markzuckerberg'}>
+        <Flex w={'full'} justifyContent={'center'} />
+        <Button mx={"auto"}>visit profile page</Button>
+        </Link>
+
+        {/* <div><CreatePost/>
         
-            </div>
-            
+            </div> */}
+            </>  
     )
 
 }
